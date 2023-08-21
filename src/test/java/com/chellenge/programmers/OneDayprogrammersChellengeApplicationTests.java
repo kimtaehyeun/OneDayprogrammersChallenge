41d3ab5a777b04715638da57176003b39a76e9b3
kimtaehyeun <rlaxogus2799@naver.com>
2023-08-21 17:32:48 +0900

    init

---
+++ src/test/java/com/chellenge/programmers/OneDayprogrammersChellengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chellenge.programmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayprogrammersChellengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
