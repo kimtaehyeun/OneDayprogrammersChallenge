a9d2c5290fe558f4f95252a4e08ee22fcd9a5dd7
kimtaehyeun <rlaxogus2799@naver.com>
2023-08-26 21:38:38 +0900

    문자열의 뒤의 n글자

--- src/main/java/chellenge/programmers/week1/friday/chellenge3.java
+++ src/main/java/chellenge/programmers/week1/friday/chellenge3.java
@@ -6,37 +6,33 @@ import java.util.List;
 /*
  *  문자열의 뒤의 n글자
 문제 설명
-길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. 
-parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 
-각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+문자열 my_string과 정수 n이 매개변수로 주어질 때,
+ my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ my_strings의 길이 = parts의 길이 ≤ 100
-1 ≤ my_strings의 원소의 길이 ≤ 100
-parts[i]를 [s, e]라 할 때, 다음을 만족합니다.
-0 ≤ s ≤ e < my_strings[i]의 길이
+my_string은 숫자와 알파벳으로 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+1 ≤ n ≤ my_string의 길이
+
 
  */
 public class chellenge3 {
 
 	public static void main(String[] args) {
 //		입출력 예
-//		my_strings												parts								result
-//		["progressive", "hamburger", "hammer", "ahocorasick"]	[[0, 4], [1, 2], [3, 5], [7, 7]]	"programmers"
+//		my_string			n		result
+//		"ProgrammerS123"	11		"grammerS123"
+//		"He110W0r1d"		5		"W0r1d"		
 		
-		
-		String[] my_strings= {"progressive","hamburger","hammer","ahocorasick"};
-		int[][] parts= {{0, 4},{1, 2},{3, 5},{7, 7}};
-		System.out.println(Solution(my_strings, parts));
+		String my_string="ProgrammerS123";
+		int n=11;
+		System.out.println(Solution(my_string, n));
 		
 	}
 	
-	public static String Solution(String[] my_strings, int[][] parts) {
+	public static String Solution(String my_string, int n) {
 		  String answer = "";
-		  for(int i=0; i<my_strings.length; i++) {
-			  answer+=my_strings[i].substring(parts[i][0],parts[i][1]+1);
-		  }
-		  
+		  answer = my_string.substring(my_string.length()-n);
 	       return answer;
 		
 		
---
+++ src/main/java/chellenge/programmers/week1/friday/chellenge4.java
@@ -0,0 +1,46 @@
+package chellenge.programmers.week1.friday;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *  접미사 배열
+문제 설명
+어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 
+예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+문자열 my_string이 매개변수로 주어질 때, my_string의 모든 접미사를 사전순으로 정렬한 문자열 배열을 
+return 하는 solution 함수를 작성해 주세요. 
+  
+ 
+ 제한사항
+my_string은 알파벳 소문자로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 100
+ *
+ */
+public class chellenge4 {
+
+	public static void main(String[] args) {
+//		my_string			result
+//		"banana"			["a", "ana", "anana", "banana", "na", "nana"]
+//		"programmers"		["ammers", "ers", "grammers", "mers", "mmers", "ogrammers", "programmers", "rammers", "rogrammers", "rs", "s"]	
+		
+		String my_string="banana";
+		
+		System.out.println(Solution(my_string));
+		
+	}
+	
+	public static String[] Solution(String my_string) {
+		String[] answer = {};
+		List<String> ar = new ArrayList();
+		for(int i=0; i<my_string.length(); i++) {
+			ar.add(my_string.substring(i));
+		}
+				
+		
+		 
+        return answer;
+		
+		
+	}
+}
