c668760b7a9f3c57b36c76121b631dd2a0c4b93c
kimtaehyeun <rlaxogus2799@naver.com>
2023-08-26 21:40:29 +0900

    접미사 배열

--- src/main/java/chellenge/programmers/week1/friday/chellenge4.java
+++ src/main/java/chellenge/programmers/week1/friday/chellenge4.java
@@ -11,7 +11,7 @@ import java.util.List;
 문자열 my_string이 매개변수로 주어질 때, my_string의 모든 접미사를 사전순으로 정렬한 문자열 배열을 
 return 하는 solution 함수를 작성해 주세요. 
   
- 
+  
  제한사항
 my_string은 알파벳 소문자로만 이루어져 있습니다.
 1 ≤ my_string의 길이 ≤ 100
