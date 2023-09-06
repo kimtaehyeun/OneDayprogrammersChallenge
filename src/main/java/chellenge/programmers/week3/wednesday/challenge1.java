2e2867530d2d8b7e187ccf0e57a609e9b9b0a029
kimtaehyeun <rlaxogus2799@naver.com>
2023-09-07 02:08:37 +0900

    구름톤 챌린지

--- src/main/java/chellenge/programmers/week3/wednesday/challenge1.java
+++ src/main/java/chellenge/programmers/week3/wednesday/challenge1.java
@@ -2,8 +2,6 @@ package chellenge.programmers.week3.wednesday;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 public class challenge1 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,15 +9,21 @@ public class challenge1 {
 		String [] dataSplit =input.split(" ");
 		int size = Integer.parseInt(dataSplit[0]);
 		int line = Integer.parseInt(dataSplit[1]);
-		List<Integer> xLine = new ArrayList<>();
-		List<Integer> yLine = new ArrayList<>();
 		
-		int point = 0;
+		int [][] x = new int[size+1][size+1];
+		int [][] y = new int[size+1][size+1];
+		
+
+		Long point = 0L;
 		for(int i=0; i<line; i++){
+			int [] data = new int[size];
+
 			String [] inputData = br.readLine().split(" ");
+
+
 			if(inputData[2].equals("R")||inputData[2].equals("L")){
 				for(int j=Integer.parseInt(inputData[1]);j<=size&&j>=1;){
-					xLine.add(Integer.parseInt(inputData[0])*10+j);
+					x[Integer.parseInt(inputData[0])][j]++;
 					if(inputData[2].equals("R")){
 						j++;
 					}
@@ -30,7 +34,8 @@ public class challenge1 {
 			}
 			else if(inputData[2].equals("U")||inputData[2].equals("D")){
 				for(int j=Integer.parseInt(inputData[0]);j<=size&&j>=1;){
-					yLine.add(j*10+Integer.parseInt(inputData[1]));
+					
+					y[j][Integer.parseInt(inputData[1])]++;
 					if(inputData[2].equals("D")){
 						j++;
 					}
@@ -39,20 +44,14 @@ public class challenge1 {
 					}		
 				}
 			}
-			else{
-				System.out.print("다시입력해주세요");
-				i--;
+		}
+		for(int i=1; i<=size; i++){
+			for(int j=1; j<=size; j++){
+				point+=x[i][j]*y[i][j];
 			}
 		}
-		for(int i=0; i<xLine.size(); i++){
-			for(int j=0; j<yLine.size(); j++){
-				if(xLine.get(i)==yLine.get(j)){
-					point++;
-				}
-			}
-		}
-		System.out.print("점 : "+point);
-		
-		
+		System.out.print(point);
+
+
 	}
 }
